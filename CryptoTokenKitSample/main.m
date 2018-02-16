491e6dac624f175fb1ae5aac5fecd15041e49d26
Pengjie Li <lpengjie@gmail.com>
2018-02-16 20:10:22 +0100

    List multiple readers

--- CryptoTokenKitSample/main.m
+++ CryptoTokenKitSample/main.m
@@ -13,8 +13,20 @@ int main(int argc, const char * argv[]) {
     mngr = [TKSmartCardSlotManager defaultManager];
     
     // Use the first reader/slot found
-    NSString *slotName = (NSString *)mngr.slotNames[0];
-    NSLog(@"slotName: %@", slotName);
+    NSArray<NSString *> *slotNames = [mngr slotNames];
+    if([slotNames count] < 1)
+    {
+        NSLog(@"No smart card reader found!");
+        return 0;
+    }
+    
+    NSUInteger index = 1;
+    for (NSString *slotName in slotNames)
+    {
+        NSLog(@"Slot %lu: %@", (unsigned long)index, slotName);
+        index++;
+    }
+    NSString *slotName = [mngr slotNames][0];
     
     // connect to the slot
     [mngr getSlotWithName:slotName reply:^(TKSmartCardSlot *slot)
@@ -30,9 +42,8 @@ int main(int argc, const char * argv[]) {
                   {
                       // send 1st APDU
                       uint8_t aid[] = {0xA0, 0x00, 0x00, 0x00, 0x63, 0x50, 0x4b, 0x43, 0x53, 0x2d, 0x31, 0x35};
-                      NSData *data = [NSData dataWithBytes: aid length: sizeof aid];
-                      [card sendIns:0xA4 p1:0x04 p2:0x0c data:data le:nil
-                              reply:^(NSData *replyData, UInt16 sw, NSError *error)
+                      NSData *data = [NSData dataWithBytes:aid length:sizeof aid];
+                      [card sendIns:0xA4 p1:0x04 p2:0x0c data:data le:nil reply:^(NSData *replyData, UInt16 sw, NSError *error)
                        {
                            if (error)
                            {
