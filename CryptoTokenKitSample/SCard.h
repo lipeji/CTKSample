96ced512911cec7cb42edc74ba36411e2e74fbdf
Pengjie Li <pli@LilyPengjiesMBP.fritz.box>
2018-02-21 15:39:24 +0100

    new classes

---
+++ CryptoTokenKitSample/SCard.h
@@ -0,0 +1,15 @@
+//
+//  SCard.h
+//  CryptoTokenKitSample
+//
+//  Created by Pengjie Li on 21.02.18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCard : NSObject
+
+- (void) run;
+
+@end
